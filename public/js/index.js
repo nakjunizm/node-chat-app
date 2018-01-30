70fafb0e3a06df1707cd86df3c7ed1c0e1a351b3
n_j.sung <n_j.sung@samsung.com>
2018-01-30 17:07:06 +0900

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
             
 socket.on('connect',  function () {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'NJ',
-        text: 'Yup, that works for me.'
-    });
 });
 
 socket.on('disconnect', function () {
