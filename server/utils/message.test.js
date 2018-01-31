8328c2317a79853b103f5b6c25a6f71e1ab49ecc
n_j.sung <n_j.sung@samsung.com>
2018-01-31 15:28:49 +0900

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -9,4 +9,12 @@ describe('generateMessage', () => {
         expect(typeof res.createdAt).toBe('number');
     });
 
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        let res = generateLocationMessage('admin',1,2);
+        expect(res.from).toBe('admin');
+        expect(res.url).toBe(`https://www.google.com/maps?q=1,2`);
+    });
 });
